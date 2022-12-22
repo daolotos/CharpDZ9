b15e8fa033186a1695957cd025496e239f0cae3b
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-22 23:35:22 +0300

    Сделала Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int M = ReadInt("M = ");
+int N = ReadInt("N = ");
+
+int summ = SummRecursive(M, N);
+
+Console.WriteLine($"Сумма чисел от {M} до {N}: {summ}");
+
+int SummRecursive(int m, int n)
+{
+    if (m > n)
+        return 0;
+    return m + SummRecursive(m + 1, n);
+}
+
+int ReadInt(string prompt)
+{
+    Console.Write(prompt);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
