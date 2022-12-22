2d7291c3912565c5dd30efb31c78d360b03725c9
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-22 23:45:44 +0300

    Сделала задачу 68

---
+++ Task68/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("вычисления функции Аккермана:");
+int m = ReadInt("m = ");
+int n = ReadInt("n = ");
+
+int a = Acermann(m, n);
+
+Console.WriteLine($"A({m},{n}) = {a}");
+
+// m и n - перепутаны местами. Реализация функции как в википедии, ввод чисел как в задании
+int Acermann(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    if (m == 0)
+        return Acermann(n - 1, 1);
+    return Acermann(n - 1, Acermann(n, m - 1));
+}
+
+int ReadInt(string prompt)
+{
+    Console.Write(prompt);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
